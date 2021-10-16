978bd695439145242d2f899c56c16487ef7d9eae
Dev4LifeV <vctudo.27@gmail.com>
2021-10-16 02:35:44 -0300

    change keyboardtype

--- lib/main.dart
+++ lib/main.dart
@@ -124,6 +124,7 @@ class KittiesApp extends StatelessWidget {
                 textSelectionTheme:
                     TextSelectionThemeData(selectionColor: Color(0xff98EFDA))),
             child: TextFormField(
+              keyboardType: TextInputType.text,
               controller: _controller.textEditingController,
               decoration: InputDecoration(
                 hintText: MediaQuery.of(context).size.width <= 768
