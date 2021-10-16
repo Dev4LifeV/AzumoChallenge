6b95818a222c937ac343cb2d9b5d937266e03ccb
Dev4LifeV <vctudo.27@gmail.com>
2021-10-16 03:19:51 -0300

    change textinputaction

--- lib/main.dart
+++ lib/main.dart
@@ -124,7 +124,6 @@ class KittiesApp extends StatelessWidget {
                 textSelectionTheme:
                     TextSelectionThemeData(selectionColor: Color(0xff98EFDA))),
             child: TextFormField(
-              textInputAction: TextInputAction.next,
               controller: _controller.textEditingController,
               decoration: InputDecoration(
                 hintText: MediaQuery.of(context).size.width <= 768
